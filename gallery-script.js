1d0115cc873426002cf2b8fd3e4d9e7aad443c93
Yaroslav Petrovskyi <yarikpetrovsk@gmail.com>
2020-09-26 22:28:11 +0300

    Выполнение доп заданий

--- gallery-script.js
+++ gallery-script.js
@@ -3,11 +3,18 @@ import galleryItems from "./gallery-items.js";
 const refs = {
   gallery: document.querySelector(".js-gallery"),
   lightBox: document.querySelector(".js-lightbox"),
+  lightBoxOverlay: document.querySelector(".js-lightbox .lightbox__overlay"),
   lightBoxImage: document.querySelector(".js-lightbox img"),
   closeLightBoxBtn: document.querySelector(
     'button[data-action="close-lightbox"]'
   ),
 };
+let carrentCard = {};
+let currentImage = {};
+let nextImage = {};
+let previousImage = {};
+let nextCard = {};
+let previousCard = {};
 
 createGallery(galleryItems);
 
@@ -34,27 +41,84 @@ function createGallery(items) {
   );
 }
 
-function onGalleryContainerClick(evt) {
-  evt.preventDefault();
+function onGalleryContainerClick(event) {
+  event.preventDefault();
 
-  if (evt.target.nodeName !== "IMG") {
+  if (event.target.nodeName !== "IMG") {
     return;
   }
 
-  refs.lightBoxImage.src = evt.target.dataset.source;
-  refs.lightBoxImage.alt = evt.target.alt;
-  openLightBox();
+  refs.lightBoxImage.src = event.target.dataset.source;
+  refs.lightBoxImage.alt = event.target.alt;
+  openLightBox(event);
 }
 
-function openLightBox() {
+function openLightBox(event) {
   refs.lightBox.classList.add("is-open");
-  refs.closeLightBoxBtn.addEventListener("click", onCloseBtnClick);
+  refs.closeLightBoxBtn.addEventListener("click", CloseLightBox);
+  refs.lightBoxOverlay.addEventListener("click", onBackdropClick);
+  window.addEventListener("keydown", onEscKeyPress);
+  window.addEventListener("keydown", onKeyLeftPress);
+  window.addEventListener("keydown", onKeyRightPress);
+  currentImage = event.target;
 }
 
-function onCloseBtnClick(evt) {
+function CloseLightBox(event) {
   refs.lightBox.classList.remove("is-open");
-  refs.closeLightBoxBtn.removeEventListener("click", onCloseBtnClick);
+  refs.closeLightBoxBtn.removeEventListener("click", CloseLightBox);
+  refs.lightBoxOverlay.removeEventListener("click", onBackdropClick);
+  window.removeEventListener("keydown", onEscKeyPress);
+  window.removeEventListener("keydown", onKeyLeftPress);
+  window.removeEventListener("keydown", onKeyRightPress);
 
   refs.lightBoxImage.src = "";
   refs.lightBoxImage.alt = "";
+}
+
+function onBackdropClick(event) {
+  if (event.target === refs.lightBoxOverlay) {
+    CloseLightBox();
+  }
+}
+
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    CloseLightBox();
+  }
+}
+
+function onKeyLeftPress(event) {
+  if (event.code !== "ArrowLeft") {
+    return;
+  }
+
+  carrentCard = currentImage.closest(".gallery__item");
+  previousCard = card.previousElementSibling;
+
+  if (previousCard === null) {
+    return;
+  }
+
+  previousImage = previousCard.querySelector("img");
+  refs.lightBoxImage.src = previousImage.dataset.source;
+  refs.lightBoxImage.alt = previousImage.alt;
+  currentImage = previousImage;
+}
+
+function onKeyRightPress(event) {
+  if (event.code !== "ArrowRight") {
+    return;
+  }
+
+  carrentCard = currentImage.closest(".gallery__item");
+  nextCard = carrentCard.nextElementSibling;
+
+  if (nextCard === null) {
+    return;
+  }
+
+  nextImage = nextCard.querySelector("img");
+  refs.lightBoxImage.src = nextImage.dataset.source;
+  refs.lightBoxImage.alt = nextImage.alt;
+  currentImage = nextImage;
 }
